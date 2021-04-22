e8921f2827d2b62ccb10ff7599ef62118d59ba33
Asus <nnimesha97@gmail.com>
2021-04-22 17:18:57 +0530

    Cruds finished

---
+++ IT18228786_Scholar_Management/src/com/Scholar_Services.java
@@ -0,0 +1,72 @@
+package com;
+
+import model.Scholar;
+
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+@Path("/Scholars")
+
+public class Scholar_Services {
+	
+	Scholar scholar =  new Scholar();
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readresearchstatus() {
+		return scholar.readresearchstatus();
+	}
+	
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertresearchstatus(@FormParam("researchID") String researchID,
+			@FormParam("progress") String progress,
+			@FormParam("comment") String comment,
+			@FormParam("approval") String approval) {
+		String output = scholar.insertresearchstatus(researchID, progress, comment, approval);
+		return output;
+	}
+
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updatesresearchstatus(String ResearchStatusData) {
+		// Convert the input string to a JSON object
+		JsonObject ResearchStatusObject = new JsonParser().parse(ResearchStatusData).getAsJsonObject();
+		// Read the values from the JSON object
+
+		String refID = ResearchStatusObject.get("refID").getAsString();
+		String researchID = ResearchStatusObject.get("researchID").getAsString();
+		String progress = ResearchStatusObject.get("progress").getAsString();
+		String comment = ResearchStatusObject.get("comment").getAsString();
+		String approval = ResearchStatusObject.get("approval").getAsString();
+		String output = scholar.updatesresearchstatus(refID, researchID, progress, comment, approval);
+		return output;
+	}
+
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteResearchStatus(String ResearchStatusData) {
+		// Convert the input string to an XML document
+		Document doc = Jsoup.parse(ResearchStatusData, "", Parser.xmlParser());
+
+		String refid = doc.select("refID").text();
+		String output = scholar.deleteresearchstatus(refid);
+		return output;
+	}
+	
+}
---
+++ IT18228786_Scholar_Management/src/model/Scholar.java
@@ -0,0 +1,142 @@
+package model;
+
+import java.sql.*;
+
+public class Scholar {
+
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/gadgetbadgetdb?useTimezone=true&serverTimezone=UTC",
+					"root", "");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return con;
+	}
+
+	public String readresearchstatus() {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for reading.";
+			}
+			// Prepare the html table to be displayed
+			output = "<table border='1'><tr><th>Ref ID</th><th>researchID</th>" + "<th>progress</th>"
+					+ "<th>comment</th>" + "<th>approval</th>" + "<th>Update</th><th>Remove</th></tr>";
+
+			String query = "select * from researchstatus";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			// iterate through the rows in the result set
+
+			//`refID``researchID``progress``comment``approval`
+			while (rs.next()) {
+				String refID = Integer.toString(rs.getInt("refID"));
+				String researchID =  Integer.toString(rs.getInt("progress"));
+				String progress = Integer.toString(rs.getInt("progress"));
+				String comment = rs.getString("comment");
+				String approval = rs.getString("approval");
+				// Add into the html table
+				output += "<td>" + refID + "</td>";
+				output += "<td>" + researchID + "</td>";
+				output += "<td>" + progress + "</td>";
+				output += "<td>" + comment + "</td>";
+				output += "<td>" + approval + "</td>";
+				// buttons
+				output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"
+						+ "<td><form method='post' action='researchstatus.jsp'>"
+						+ "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"
+						+ "<input name='sid' type='hidden' value='" + refID + "'>" + "</form></td></tr>";
+			}
+			con.close();
+			// Complete the html table
+			output += "</table>";
+		} catch (Exception e) {
+			output = "Error while reading ";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	public String insertresearchstatus(String researchID,String progress,String comment,String approval) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for inserting.";
+			}
+			// create a prepared statement
+			String query = " insert into researchstatus (`refID`,`researchID`,`progress`,`comment`,`approval`)"
+					+ " values (?, ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setInt(2,Integer.parseInt(researchID));
+			preparedStmt.setInt(3,Integer.parseInt(progress));
+			preparedStmt.setString(4, comment);
+			preparedStmt.setString(5, approval);
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Inserted successfully";
+		} catch (Exception e) {
+			output = "Error while inserting";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	public String updatesresearchstatus(String refID,String researchID,String progress,String comment,String approval) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for updating.";
+			}
+			// create a prepared statement
+			String query = "UPDATE researchstatus SET researchID=?,progress=?,comment=?,approval=? WHERE refID=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1,Integer.parseInt(researchID));
+			preparedStmt.setInt(2,Integer.parseInt(progress));
+			preparedStmt.setString(3, comment);
+			preparedStmt.setString(4, approval);
+			preparedStmt.setInt(5, Integer.parseInt(refID));
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Updated successfully";
+		} catch (Exception e) {
+			output = "Error while updating";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	public String deleteresearchstatus(String refID) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for deleting.";
+			}
+			// create a prepared statement
+			String query = "delete from researchstatus where refID=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, Integer.parseInt(refID));
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Deleted successfully";
+		} catch (Exception e) {
+			output = "Error while deleting";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+}
